6dd67aba54d3bf343c0c13fea86368cf9649a44d
Mr. Sun <futiliter@Futiliters-MacBook-Pro.local>
2020-11-03 22:59:47 -0500

    Modified work

--- Assignment3B/Assignment3B/main.cpp
+++ Assignment3B/Assignment3B/main.cpp
@@ -7,44 +7,41 @@
 #include <string>
 using namespace std;
 
-void testPalindrome(bool, string);
+void testPalindrome(bool, string, string, int); //Function prototyping
 
 int main() {
+    
+    //Declaring variables
     int i;
     string sentence;
     string reverse = "";
+    bool palindrome;
     
     cout << "Enter a string ";
     getline(cin, sentence);
     
     
-    for (i = 0; sentence[i]; i++) {
-        
-            cout << sentence[i];
-        
-    }
-    
-    for (i = sentence.length()-1; i >= 0; i--){
-        
-        reverse = reverse + sentence[i];
-        cout << reverse << endl;
-    }
-    
-    bool palindrome = true;
-    
-    for (i = 0; i < sentence.length(); i++){
-        if (sentence[i] != reverse[i]){
-            palindrome = false;
-        }
-    }
-    
-    testPalindrome(palindrome, sentence);
+    testPalindrome(palindrome, sentence, reverse, i); //Calling function
     
     return 0;
 }
 
-void testPalindrome (bool palindrome, string sentence){
+//Function that outputs the result of the palindrome test
+void testPalindrome (bool palindrome, string sentence, string reverse, int i){
     
+    
+    for (i = sentence.length()-1; i >= 0; i--){ //iterating and showing the word in reverse
+        
+        reverse = reverse + sentence[i];
+    }
+    
+     palindrome = true; //default
+    
+    for (i = 0; i < sentence.length(); i++){ //verify if condition is still true or if needs to change
+        if (sentence[i] != reverse[i]){
+            palindrome = false;
+        }
+    }
       
     if (palindrome) {
         cout << sentence << " is a palindrome !" << endl;
